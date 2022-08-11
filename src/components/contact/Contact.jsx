dca2584b586db59a3794295f1f423a89e8cbd878
Ivan <ivanbelarmino361@gmail,com>
2022-08-11 20:10:57 +0800

    initial commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import "./contact.css"
+import React from "react";
+import "./contact.css";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section className="contact container section" id="contact">
+      <h2 className="section-title">Let Me Know</h2>
+      <span className="section subtitle">Contact us to know it </span>
 
-export default Contact
+      <div className="contact-container grid">
+        <div className="contact-info">
+          <h3 className="contact-title">Lorem, ipsum dolor.</h3>
+          <p className="contact-details">Lorem ipsum dolor sit amet.</p>
+        </div>
+        <form action="" className="contact-form">
+          <div className="contact_form-group">
+            <div className="contact_form-div">
+              <input
+                type="text"
+                className="contact_form-contact"
+                placeholder="Your name?"
+              />
+            </div>
+            <div className="contact_form-div">
+              <input
+                type="email"
+                className="contact_form-contact"
+                placeholder="What is your Email?"
+              />
+            </div>
+            <div className="contact_form-div">
+              <textarea
+                name=""
+                id=""
+                cols="30"
+                rows="10"
+                placeholder="Spill your message and we chitchat it"
+                className="contact_form-contact"
+              ></textarea>
+              <button className="btn">Send the Message</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/components/testemony/Testemony.jsx
+++ src/components/testemony/Testemony.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
-import "./testemony.css"
+import React from "react";
+import "./testemony.css";
+import Image1 from "../../assets/avatar-1.svg";
+import Image3 from "../../assets/avatar-3.svg";
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+
+const data = [
+  {
+    id: 1,
+    image: Image1,
+    name: "John Doe",
+    subtitle: "Web Developer",
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.",
+  },
+  {
+    id: 2,
+    image: Image3,
+    name: "John Doe",
+    subtitle: "Web Developer",
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.",
+  },
+];
 
 const Tesemony = () => {
   return (
-    <div>Tesemony</div>
-  )
-}
+    <section className="testemony container section">
+      <h2 className="section-title">Clients & Reviews</h2>
 
-export default Tesemony
+      <Swiper
+        className="testemony-container grid"
+        modules={[Pagination]}
+        spaceBetween={30}
+        slidesPerView={1}
+        loop={true}
+        grabCursor={true}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ id, image, name, subtitle, comment }) => {
+          return (
+            <SwiperSlide className="testemony-item" key={id}>
+              <div className="thumb">
+                <img src={image} alt="" />
+              </div>
+              <h3 className="testemony-title">{name}</h3>
+              <span className="subtitle">{subtitle}</span>
+              <div className="comment">{comment}</div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Tesemony;
