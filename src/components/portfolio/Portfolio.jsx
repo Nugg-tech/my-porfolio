4f78eb7daf14b8646fd628c73a2c4d370c67c624
ivan <ivanbelarmino361@gmail.com>
2022-08-17 23:51:57 +0800

    initial commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -16,7 +16,7 @@ const Portfolio = () => {
       <h2 className="section-title">Recent Works</h2>
 
       <div className="work-filter">
-        <span className="work-item" onClick={() => setItem(Menu)}>Everything</span>
+        <span className="work-item" onClick={() => setItems(Menu)}>Everything</span>
         <span className="work-item" onClick={() => filterItem("Creative")}>Creative</span>
         <span className="work-item" onClick={() => filterItem("Art")}>Art</span>
         <span className="work-item" onClick={() => filterItem("Design")}>Design</span>
